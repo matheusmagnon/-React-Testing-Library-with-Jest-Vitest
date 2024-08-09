af7845d29926f6d418f9d96169fc1c7c6232873c
Matheus Magno <matheusmagnon@gmail.com>
2024-08-09 14:51:36 -0300

    feat: create component Options

---
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import Row from 'react-bootstrap/Row';
+
+import ScoopOption from './ScoopOption';
+
+export default function Options({ optionType }) {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        console.log('optionType', optionType)
+        axios.get(`http://localhost:3030/${optionType}`)
+            .then((response) => {
+                setItems(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [optionType]);
+
+    const ItemCoponent = optionType === 'scoops' ? ScoopOption : null;
+
+    const optionsItems = items.map(item => <ItemCoponent key={item.name} name={item.name} imagePath={item.imagePath} />)
+    return <Row>{optionsItems}</Row>
+}
